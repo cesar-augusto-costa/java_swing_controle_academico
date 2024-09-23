40c9c443a5baf2e5980b14ecbc8471fb9f8a9c06
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2024-09-22 21:56:55 -0300

    prof: Menu Principal

---
+++ src/prof/ArrayInterface/MenuPrincipal.java
@@ -0,0 +1,65 @@
+package prof.ArrayInterface;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MenuPrincipal extends JFrame {
+
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MenuPrincipal frame = new MenuPrincipal();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public MenuPrincipal() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 400, 200);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        
+        JButton btnCadastroAluno = new JButton("Cadastrar Aluno");
+        btnCadastroAluno.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                CadastroAlunos frameAluno = new CadastroAlunos();
+                frameAluno.setVisible(true);
+            }
+        });
+        btnCadastroAluno.setBounds(10, 50, 164, 50);
+        contentPane.add(btnCadastroAluno);
+
+        
+        JButton btnCadastroProfessor = new JButton("Cadastrar Professor");
+        btnCadastroProfessor.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                CadastroProfessor frameProfessor = new CadastroProfessor();
+                frameProfessor.setVisible(true);
+            }
+        });
+        btnCadastroProfessor.setBounds(196, 50, 178, 50);
+        contentPane.add(btnCadastroProfessor);
+    }
+}
+
